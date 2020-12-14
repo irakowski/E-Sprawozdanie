7b6f5184a93bb963b4d875abaa37a3e72a488339
irakowski <inna.rakowski@gmail.com>
2020-12-14 20:56:24 +0100

    Thousands template_tag

--- ESFViewer/templatetags/viewer_tags.py
+++ ESFViewer/templatetags/viewer_tags.py
@@ -5,3 +5,12 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def thousands(value):
+    """Formats number values with blank space thousand separator"""
+    try:
+        value = float(value)
+    except ValueError:
+        return value
+    return f"{value:,}".replace(',',' ')
