008f810d483f98e47258e42ff0f75b6e9c74acc7
irakowski <inna.rakowski@gmail.com>
2020-12-17 23:52:49 +0100

    cases

--- ESFViewer/xml_parse.py
+++ ESFViewer/xml_parse.py
@@ -24,7 +24,7 @@ def data_list(el_tag, root):
             index = tag_with_namespace.rfind('}')
             tag = tag_with_namespace[index+1:]
             text = child.text.strip()
-            if text == "":
+            if text == '':
                 l = list((tag,))
                 d.append(l)
             else:
@@ -78,84 +78,84 @@ def parse_txt(text):
     intro_element = root.find('tns:WprowadzenieDoSprawozdaniaFinansowego', my_namespaces)
     intro = data_list('tns:WprowadzenieDoSprawozdaniaFinansowego', root)
     for item in intro:
-        if "P_1A" in item:
+        if 'P_1A' in item:
             item = item[1:]
             elements['podmiot'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
-        if "Siedziba" in item:
+        if 'Siedziba' in item:
             item = item[1:]
             elements['siedziba'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
-        if "Adres" in item:
+        if 'Adres' in item:
             item = item[1:]
             elements['adres'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
-        if "AdresPrzedsiebiorcyZagranicznego" in item:
+        if 'AdresPrzedsiebiorcyZagranicznego' in item:
             item = item[1:]
             elements['adres_zagran'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
     #PKD
     pkd_el = root.findall('.//tns:P_1C', my_namespaces)
     if pkd_el:
         for el in pkd_el:
-            elements["pkd"] = [pkd.strip() for pkd in el.itertext() if pkd.strip()]
+            elements['pkd'] = [pkd.strip() for pkd in el.itertext() if pkd.strip()]
 
     #NIP
     nip_el = root.findall('.//tns:P_1D', my_namespaces)
     if nip_el:
-        elements["nip"] = [el.text.strip() for el in nip_el][0]
+        elements['nip'] = [el.text.strip() for el in nip_el][0]
     else:
         elements['nip'] = None
    
     #KRS
     krs_el = root.findall('.//tns:P_1E', my_namespaces)
     if krs_el:
-        elements["krs"] = [el.text.strip() for el in krs_el][0]
+        elements['krs'] = [el.text.strip() for el in krs_el][0]
     else:
-        elements["krs"] = None
+        elements['krs'] = None
 
     elements['czas_dzialalnosci_jezeli_ograniczony'] = intro_element.find('tns:P_2', my_namespaces)
     elements['okres_sprawozdania'] = make_dict(data_list('tns:P_3', intro_element))
-    elements["dane_laczne"] = str_to_bool(intro_element.find('tns:P_4', my_namespaces).text.strip())
+    elements['dane_laczne'] = str_to_bool(intro_element.find('tns:P_4', my_namespaces).text.strip())
     
     kontynuacja_dzialalnosci = make_dict(data_list('tns:P_5', intro_element))
     kontynuacja_dzialalnosci['P_5A'] = str_to_bool(kontynuacja_dzialalnosci.get('P_5A'))
     kontynuacja_dzialalnosci['P_5B'] = str_to_bool(kontynuacja_dzialalnosci.get('P_5B'))
-    elements["kontynuacja_dzialalnosci"] = kontynuacja_dzialalnosci
+    elements['kontynuacja_dzialalnosci'] = kontynuacja_dzialalnosci
 
     info_o_polaczeniu = data_list('tns:P_6', intro_element)
     if info_o_polaczeniu is not None:
         info_o_polaczeniu = make_dict(info_o_polaczeniu)
         info_o_polaczeniu['P_6A'] = str_to_bool(info_o_polaczeniu.get('P_6A'))
-        elements["info_o_polaczeniu"] = info_o_polaczeniu
+        elements['info_o_polaczeniu'] = info_o_polaczeniu
 
     zasady = intro_element.find('tns:P_7', my_namespaces)
-    zasady_text = ""
+    zasady_text = ''
     for el in zasady.itertext():
         zasady_text = zasady_text + el.strip().replace(u'\xa0', u' ')
-    elements["zasady"] = zasady_text
+    elements['zasady'] = zasady_text
     zalozenia = intro_element.find('tns:P_8', my_namespaces)
-    zalozenia_text = ""
+    zalozenia_text = ''
     if zalozenia is not None:
         for line in zalozenia.itertext():
             zalozenia_text = zalozenia_text + line.strip().replace(u'\xa0', u' ')
     elements['zalozenia'] = zalozenia_text   
-    elements["Bilans"] = data_list('tns:Bilans', root)
+    elements['Bilans'] = data_list('tns:Bilans', root)
     elements['rzis'] = data_list('tns:RZiS', root)
-    elements["zmiany"] = data_list('tns:ZestZmianWKapitale', root)
-    elements["rachunek"] = data_list('tns:RachPrzeplywow', root)
+    elements['zmiany'] = data_list('tns:ZestZmianWKapitale', root)
+    elements['rachunek'] = data_list('tns:RachPrzeplywow', root)
 
     ##CALCULATING RESULTS
-    bilans_data = calculation_dict(elements, "Bilans", ["Aktywa", "Pasywa_A"])
-    aktywa_razem = bilans_data.get("Aktywa", 0)
-    kapital_fundusz_wlasny = bilans_data.get("Pasywa_A", 0)
+    bilans_data = calculation_dict(elements, 'Bilans', ['Aktywa', 'Pasywa_A'])
+    aktywa_razem = bilans_data.get('Aktywa', 0)
+    kapital_fundusz_wlasny = bilans_data.get('Pasywa_A', 0)
 
-    report_data = calculation_dict(elements, 'rzis',[ "F", "I", "L", "A", "A_II", "A_III", "E", "D", "G"])
-    zysk_strata_z_dz_operacyjnej = report_data.get("F", 0)
-    zysk_strata_brutto = report_data.get("I", 0)
-    zysk_strata_netto = report_data.get("L", 0)
-    przychody_netto_ze_sprzedazy = report_data.get("A", 0)
-    zmiana_stanu_produktow = report_data.get("A_II", 0)
-    koszt_wytworzenia_na_wlasne_potrzeby = report_data.get("A_III",0)
-    pozostale_koszty_operacyjne = report_data.get("E", 0)
-    pozostale_przychody_operacyjne = report_data.get("D", 0)
-    przychody_finansowe = report_data.get("G", 0)
+    report_data = calculation_dict(elements, 'rzis',[ 'F', 'I', 'L', 'A', 'A_II', 'A_III', 'E', 'D', 'G'])
+    zysk_strata_z_dz_operacyjnej = report_data.get('F', 0)
+    zysk_strata_brutto = report_data.get('I', 0)
+    zysk_strata_netto = report_data.get('L', 0)
+    przychody_netto_ze_sprzedazy = report_data.get('A', 0)
+    zmiana_stanu_produktow = report_data.get('A_II', 0)
+    koszt_wytworzenia_na_wlasne_potrzeby = report_data.get('A_III',0)
+    pozostale_koszty_operacyjne = report_data.get('E', 0)
+    pozostale_przychody_operacyjne = report_data.get('D', 0)
+    przychody_finansowe = report_data.get('G', 0)
 
     ##WARIANT POROWNAWCZY i CALCULACYJNY
     rentownosc_aktywow = (zysk_strata_netto*100) / aktywa_razem
