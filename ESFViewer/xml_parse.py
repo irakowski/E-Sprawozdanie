1ef7d02096acce434941fa803afd901be04aad98
irakowski <inna.rakowski@gmail.com>
2021-01-12 23:00:34 +0100

    FinancialStatementClass/Parser

--- ESFViewer/xml_parse.py
+++ ESFViewer/xml_parse.py
@@ -1,184 +1,205 @@
-import xml.etree.ElementTree as ET
-from esprawozdanie import settings
-#my_namespaces = dict([node for _, node in ET.iterparse(file, events=['start-ns'])])
+import re
+from bs4 import BeautifulSoup
+from . import element_mappings
+from . import validators
 
-my_namespaces = {'tns': 'http://www.mf.gov.pl/schematy/SF/DefinicjeTypySprawozdaniaFinansowe/2018/07/09/JednostkaInnaWTysiacach', 
-                'dtsf': 'http://www.mf.gov.pl/schematy/SF/DefinicjeTypySprawozdaniaFinansowe/2018/07/09/DefinicjeTypySprawozdaniaFinansowe/', 
-                'jin': 'http://www.mf.gov.pl/schematy/SF/DefinicjeTypySprawozdaniaFinansowe/2018/07/09/JednostkaInnaStruktury', 
-                'xsi': 'http://www.w3.org/2001/XMLSchema-instance', 
-                'ds': 'http://www.w3.org/2000/09/xmldsig#', 
-                'xades': 'http://uri.etsi.org/01903/v1.3.2#'}
+class FinancialStatement:
+    AVAILABLE_TYPES = (
+		'JednostkaInna',
+		'JednostkaOp', 
+	)
+    
+    def __init__(self, xml_text):
+        self.xml_text = xml_text
+        self.soup = self._cook()
+        #Nodes
+        self._naglowek = self.soup.find('Naglowek')
+        self._wprowadzenie = self._naglowek.find_next_sibling()
+        self._bilans = self._wprowadzenie.find_next_sibling()
+        self._rzis = self._bilans.find_next_sibling()
+        self._zestawienie_zmian =self.soup.find(re.compile("ZestZmianWKapit.*"))
+        self._rachunek_przeplywow = self.soup.find(re.compile("RachPrzeplyw.*"))
+        
+        #common structure for all documents
+        self.naglowek_data = self.simple_dict(self.data_list(self._naglowek))
+        self.bilans_data = self.convert_to_dict(self._bilans)
+        self.rzis_data = self.convert_to_dict(self._rzis)
+        self.zestawienie_zmian_data = self.convert_to_dict(self._zestawienie_zmian)
+        self.rachunek_przeplywow_data = self.convert_to_dict(self._rachunek_przeplywow)
 
-def data_list(el_tag, root):
-    """
-    Finds the first subelement matching el_tag. 
-    If element is found in the XML: creates a list of child elements lists. 
-    Nested children are appended to the child list.
-    Returns None if no element with given el_tag is Found in the document
-    """
-    element = root.find(el_tag, my_namespaces)
-    if element is not None:
-        d = list()
-        for child in element.iter():
-            tag_with_namespace = child.tag
-            index = tag_with_namespace.rfind('}')
-            tag = tag_with_namespace[index+1:]
-            text = child.text.strip()
-            if text == '':
-                l = list((tag,))
-                d.append(l)
-            else:
-                l.extend(list((tag, text)))
-        return d
-    else:
-        return None
+        if self.get_entity_type() in self.__class__.AVAILABLE_TYPES:
+            self.wprowadzenie_data = self.wprowadzenie_data_PKD()
+        else:
+            self.wprowadzenie_data = self.wprowadzenie_data_wo_PKD()
+        #calculaion method
+        self.method = self.get_method()
 
-def make_dict(list_data):
-    """
-    Iterates throught given list and creates dictionary 
-    """
-    for el in list_data:
-        el = el[1:]
-        return {el[i]: el[i+1] for i in range(0, len(el), 2)}
+    def _cook(self):
+        soup = BeautifulSoup(self.xml_text, 'lxml-xml')
+        return soup
+    
+    def get_entity_type(self):
+        return self.soup.contents[0].name
+        #Naglowek.KodSpawozdania ##'SprFinJednostkaInnaWZlotych'
 
-def str_to_bool(string):
-    """Converts XML boolean values(true, false) to Python bool"""
-    return string == 'true'
+    def get_method(self):   
+        porow = re.search(r'((p|P)orówn)|(RZiSPor)',str(self.soup))
+        if porow:
+            return 'RZiSPor'
+        
+    def data_list(self, main_node):
+        data = []
+        unit = []
+        for node in main_node.descendants:
+            if node.name is not None:
+                if node.string is None:
+                    unit = list((node.name,))
+                    data.append(unit)
+                else:
+                    unit.extend([node.name, node.string.strip().replace(u'\xa0', u' ')])
+        if data:
+            return data
+        return unit
 
-def calculation_dict(dictionary, key, list_el):
-    """
-    Matches search elements from list_el with parsed data, creating a dictionary 
-    with only searched elements and values needed for calculation, converted to float type
-    Returns a dict with data. 
-    >>> calculation_data = calculation_dict(elements, 'rzis', ['A', 'A_II', 'A_III'])
-    >>> {'A': 11048169.0, 'A_II': 0.0, 'A_III': 0.0 }
-    """
-    bilans_data = dict()
-    for el in dictionary[key]:
-        for let in list_el:
-            if let in el:
-                d = {el[0]: float(el[2])}
-                bilans_data.update(d)
-    return bilans_data
+    def simple_dict(self, lst):
+        """
+        Iterates throught given list where len(list)%2 == 0 and returns a dictionary  
+        """
+        return {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+
+    def convert_to_dict(self, node):
+        if node is not None:
+            l = self.data_list(node)
+            container = {}
+            for node in l:
+                if len(node) > 1:
+                    container[node[0]] = self.simple_dict(node[1:])
+            return container
+        return ""
+    
+    def wprowadzenie_data_PKD(self):
+        """
+        Iterates throught wprowadzenie 
+        """
+        main_dict = dict()
+        mapp = {'P_1A': 'Nazwa_siedziba','P_1C': 'PKD','P_1D': 'NIP','P_1E': 'KRS', 'P_2':'OgranicCzasDzialalnosci',
+                'P_3':'ZakresDat','P_4': 'SprFinDaneLaczne', 'P_5': 'KontynuacjaDzialalnosci',
+                'P_6': 'SprFinPoPolaczeniu', 'P_7': 'Zasady', 
+                'P_8': 'Zalożenia'}
+        for node in self._wprowadzenie.descendants:
+            inner_dict = {}
+            if node.name:
+                if re.match(r'P_[14][DE]*', node.name):
+                    main_dict[mapp.get(node.name, node.name)] = node.string
+                if re.match(r'P_1[CA]', node.name) or re.match(r'Adres.?', node.name) or re.match(r'P_[235678]$', node.name):
+                    for n in node.descendants:
+                        if n.name and n.string:
+                            inner_dict[n.name] = inner_dict.get(n.name, "") + n.string.strip().replace(u'\xa0', u' ') + " "
+                    main_dict[mapp.get(node.name, node.name)] = inner_dict
+        return main_dict
+
+    def wprowadzenie_data_wo_PKD(self):
+        """
+        Iterates throught wprowadzenie 
+        """
+        main_dict = dict()
+        mapp = {'P_1A': 'Nazwa_siedziba','P_1C': 'NIP','P_1D': 'KRS','P_3':'ZakresDat',
+                'P_4': 'Uproszczenia', 'P_5': 'Kontynuacja','P_6': 'Zasady'}
+        for node in self._wprowadzenie.descendants:
+            inner_dict = {}
+            if node.name:
+                if re.match(r'P_[14][CD]*', node.name):
+                    main_dict[mapp.get(node.name, node.name)] = node.string
+                if re.match(r'P_1[A]', node.name) or re.match(r'Adres.?', node.name) or re.match(r'P_[345678]$', node.name):
+                    for n in node.descendants:
+                        if n.name and n.string:
+                            inner_dict[n.name] = inner_dict.get(n.name, "") + n.string.strip().replace(u'\xa0', u' ') + " "
+                    main_dict[mapp.get(node.name, node.name)] = inner_dict
+        return main_dict
+
+    def data(self):
+        el = {}
+        el['Naglowek'] = self.naglowek_data
+        el['Wprowadzenie'] = self.wprowadzenie_data
+        el['Bilans'] = self.bilans_data
+        el['RZiS'] = self.rzis_data
+        el['ZestawienieZmian'] = self.zestawienie_zmian_data 
+        el['RachunekPrzeplywow'] = self.rachunek_przeplywow_data
+        return el
+
+    def dict_lookup(self, dictionary, value):
+        lookup_value = dictionary.get(value, None)
+        if lookup_value is not None:
+            return float(lookup_value.get('KwotaA'))
+        return 0
+
+    def marzy(self):
+        zysk_strata_z_dz_operacyjnej = self.dict_lookup(self.rzis_data, 'F')
+        zysk_strata_brutto = self.dict_lookup(self.rzis_data, 'I')
+        zysk_strata_netto = self.dict_lookup(self.rzis_data, 'L')
+        przychody_netto_ze_sprzedazy = self.dict_lookup(self.rzis_data, 'A')
+        zmiana_stanu_produktow = self.dict_lookup(self.rzis_data, 'A_II')
+        koszt_wytworzenia_na_wlasne_potrzeby = self.dict_lookup(self.rzis_data, 'A_III')
+        pozostale_koszty_operacyjne = self.dict_lookup(self.rzis_data,'E')
+        pozostale_przychody_operacyjne = self.dict_lookup(self.rzis_data, 'D')
+        przychody_finansowe = self.dict_lookup(self.rzis_data, 'G')
+        if self.method == 'RZiSPor':
+            divider_1 = przychody_netto_ze_sprzedazy-zmiana_stanu_produktow-koszt_wytworzenia_na_wlasne_potrzeby
+            divider_2 = divider_1+pozostale_przychody_operacyjne
+            try:
+                marza_operacyjna = zysk_strata_z_dz_operacyjnej*100/divider_2
+            except:
+                marza_operacyjna = None
+
+            try:
+                marza_zysku_brutto = (zysk_strata_brutto*100) / (divider_2 + przychody_finansowe)
+            except:
+                marza_zysku_brutto = None
+        else:
+            try:
+                marza_operacyjna = zysk_strata_z_dz_operacyjnej*100/ (przychody_netto_ze_sprzedazy+pozostale_przychody_operacyjne)
+            except:
+                marza_operacyjna = None
+            try:
+                marza_zysku_brutto = (zysk_strata_brutto*100)/(przychody_netto_ze_sprzedazy + pozostale_przychody_operacyjne + przychody_finansowe)
+            except:
+                marza_zysku_brutto = None
+        return marza_operacyjna, marza_zysku_brutto
+
+    def marza_operacyjna(self):
+        return self.marzy()[0]
+    
+    def marza_zysku_brutto(self):
+        return self.marzy()[1]
+
+    def rentownosc_aktywow(self):
+        zysk_strata_netto = self.dict_lookup(self.rzis_data, 'L')
+        aktywa_razem = self.dict_lookup(self.bilans_data, 'Aktywa')
+        try:
+            return zysk_strata_netto*100/ aktywa_razem
+        except:
+            return None
+    
+    def rentownosc_kapilu_wlasnego(self):
+        zysk_strata_netto = self.dict_lookup(self.rzis_data, 'L')
+        kapital_fundusz_wlasny = self.dict_lookup(self.bilans_data, 'Pasywa_A')
+        try:
+            return zysk_strata_netto*100/kapital_fundusz_wlasny
+        except:
+            return None
 
 
 def parse_txt(text):
-    """
-    Parses XML string, extracts data needed for calculation, 
-    returns a dictionary with data and calculation results
-    """
-    elements = dict()
-    root = ET.fromstring(text)
-
-    #NAGLOWEK SPRAWOZDANIA
-    naglowek = data_list('tns:Naglowek', root)
-    elements['naglowek'] = make_dict(naglowek)
     
-    #WPROWADZENIE DO SPRAWOZDANIA
-    intro_element = root.find('tns:WprowadzenieDoSprawozdaniaFinansowego', my_namespaces)
-    intro = data_list('tns:WprowadzenieDoSprawozdaniaFinansowego', root)
-    for item in intro:
-        if 'P_1A' in item:
-            item = item[1:]
-            elements['podmiot'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
-        if 'Siedziba' in item:
-            item = item[1:]
-            elements['siedziba'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
-        if 'Adres' in item:
-            item = item[1:]
-            elements['adres'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
-        if 'AdresPrzedsiebiorcyZagranicznego' in item:
-            item = item[1:]
-            elements['adres_zagran'] = {item[i]: item[i+1] for i in range(0, len(item), 2)}
-    #PKD
-    pkd_el = root.findall('.//tns:P_1C', my_namespaces)
-    if pkd_el:
-        for el in pkd_el:
-            elements['pkd'] = [pkd.strip() for pkd in el.itertext() if pkd.strip()]
+    instance = FinancialStatement(text)
+#    print(instance.get_entity_type())
+#    print(instance.method)
+    data = instance.data()
+    data['marza_operacyjna'] = instance.marza_operacyjna()
+    data['marza_zysku_brutto'] = instance.marza_zysku_brutto()
+    data['rentownosc_aktywow'] = instance.rentownosc_aktywow()
+    data['rentownosc_kapitalu_wlasnego'] = instance.rentownosc_kapilu_wlasnego()
+    return data
 
-    #NIP
-    nip_el = root.findall('.//tns:P_1D', my_namespaces)
-    if nip_el:
-        elements['nip'] = [el.text.strip() for el in nip_el][0]
-    else:
-        elements['nip'] = None
-   
-    #KRS
-    krs_el = root.findall('.//tns:P_1E', my_namespaces)
-    if krs_el:
-        elements['krs'] = [el.text.strip() for el in krs_el][0]
-    else:
-        elements['krs'] = None
 
-    elements['czas_dzialalnosci_jezeli_ograniczony'] = intro_element.find('tns:P_2', my_namespaces)
-    elements['okres_sprawozdania'] = make_dict(data_list('tns:P_3', intro_element))
-    elements['dane_laczne'] = str_to_bool(intro_element.find('tns:P_4', my_namespaces).text.strip())
-    
-    kontynuacja_dzialalnosci = make_dict(data_list('tns:P_5', intro_element))
-    kontynuacja_dzialalnosci['P_5A'] = str_to_bool(kontynuacja_dzialalnosci.get('P_5A'))
-    kontynuacja_dzialalnosci['P_5B'] = str_to_bool(kontynuacja_dzialalnosci.get('P_5B'))
-    elements['kontynuacja_dzialalnosci'] = kontynuacja_dzialalnosci
 
-    info_o_polaczeniu = data_list('tns:P_6', intro_element)
-    if info_o_polaczeniu is not None:
-        info_o_polaczeniu = make_dict(info_o_polaczeniu)
-        info_o_polaczeniu['P_6A'] = str_to_bool(info_o_polaczeniu.get('P_6A'))
-        elements['info_o_polaczeniu'] = info_o_polaczeniu
-
-    zasady = intro_element.find('tns:P_7', my_namespaces)
-    zasady_text = ''
-    for el in zasady.itertext():
-        zasady_text = zasady_text + el.strip().replace(u'\xa0', u' ')
-    elements['zasady'] = zasady_text
-    zalozenia = intro_element.find('tns:P_8', my_namespaces)
-    zalozenia_text = ''
-    if zalozenia is not None:
-        for line in zalozenia.itertext():
-            zalozenia_text = zalozenia_text + line.strip().replace(u'\xa0', u' ')
-    elements['zalozenia'] = zalozenia_text   
-    elements['Bilans'] = data_list('tns:Bilans', root)
-    elements['rzis'] = data_list('tns:RZiS', root)
-    elements['zmiany'] = data_list('tns:ZestZmianWKapitale', root)
-    elements['rachunek'] = data_list('tns:RachPrzeplywow', root)
-
-    ##CALCULATING RESULTS
-    bilans_data = calculation_dict(elements, 'Bilans', ['Aktywa', 'Pasywa_A'])
-    aktywa_razem = bilans_data.get('Aktywa', 0)
-    kapital_fundusz_wlasny = bilans_data.get('Pasywa_A', 0)
-
-    report_data = calculation_dict(elements, 'rzis',[ 'F', 'I', 'L', 'A', 'A_II', 'A_III', 'E', 'D', 'G'])
-    zysk_strata_z_dz_operacyjnej = report_data.get('F', 0)
-    zysk_strata_brutto = report_data.get('I', 0)
-    zysk_strata_netto = report_data.get('L', 0)
-    przychody_netto_ze_sprzedazy = report_data.get('A', 0)
-    zmiana_stanu_produktow = report_data.get('A_II', 0)
-    koszt_wytworzenia_na_wlasne_potrzeby = report_data.get('A_III',0)
-    pozostale_koszty_operacyjne = report_data.get('E', 0)
-    pozostale_przychody_operacyjne = report_data.get('D', 0)
-    przychody_finansowe = report_data.get('G', 0)
-
-    ##WARIANT POROWNAWCZY i CALCULACYJNY
-    rentownosc_aktywow = (zysk_strata_netto*100) / aktywa_razem
-    rentownosc_kapilu_wlasnego = (zysk_strata_netto*100)/kapital_fundusz_wlasny
-
-    if any([el for el in elements['rzis'] if 'RZiSPor' in el]):
-        #WARIANT POROWNAWCZY
-        divided_by = przychody_netto_ze_sprzedazy-zmiana_stanu_produktow-koszt_wytworzenia_na_wlasne_potrzeby+pozostale_przychody_operacyjne 
-        marza_operacyjna = (zysk_strata_z_dz_operacyjnej*100)/divided_by
-        div = divided_by + przychody_finansowe
-        marza_zysku_brutto = (zysk_strata_brutto*100)/div
-
-        elements['marza_operacyjna'] = marza_operacyjna
-        elements['marza_zysku_brutto'] = marza_zysku_brutto
-        elements['rentownosc_aktywow'] = rentownosc_aktywow
-        elements['rentownosc_kapitalu_wlasnego'] = rentownosc_kapilu_wlasnego
-    else:
-        #WARIANT CALCULACYJNY
-        calc_marza_operacyjna = (zysk_strata_z_dz_operacyjnej*100)/ (przychody_netto_ze_sprzedazy+pozostale_przychody_operacyjne)
-        calc_marza_zysku_brutto = (zysk_strata_brutto*100)/(przychody_netto_ze_sprzedazy + pozostale_przychody_operacyjne + przychody_finansowe)
-        elements['marza_operacyjna'] = calk_marza_operacyjna
-        elements['marza_zysku_brutto'] = calc_marza_zysku_brutto
-        elements['rentownosc_aktywow'] = rentownosc_aktywow
-        elements['rentownosc_kapitalu_wlasnego'] = rentownosc_kapilu_wlasnego
-
-    return elements
